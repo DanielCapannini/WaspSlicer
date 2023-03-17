f8288bd04014517624ceeb805c2aa0d08d0dd655
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-03-17 16:37:34 +0000

    correzione IMPORTANTE per il range temperatura

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -2078,12 +2078,18 @@ LayerResult GCode::process_layer(
                     continue;
             }
             int temperature = print.config().temperature.get_at(extruder.id());
+            if(print.config().layer_temperature.get_at(extruder.id()) != 0 && print.config().layer_range_min.get_at(extruder.id()) != 0 && print.config().layer_range_max.get_at(extruder.id()) != 0 ){
+                int temp_range = print.config().layer_temperature.get_at(extruder.id());
+                int range_min = print.config().layer_range_min.get_at(extruder.id());
+                int range_max = print.config().layer_range_max.get_at(extruder.id())
+            }
             if (temperature > 0 && (temperature != print.config().first_layer_temperature.get_at(extruder.id())))
-                gcode += m_writer.set_temperature(temperature, false, extruder.id());
+                range_min < m_layer_index && m_layer_index < range_max ? gcode += m_writer.set_temperature(temp_range, false, extruder.id()) : gcode += m_writer.set_temperature(temperature, false, extruder.id());
+            
         }
         gcode += m_writer.set_bed_temperature(print.config().bed_temperature.get_at(first_extruder_id));
         // Mark the temperature transition from 1st to 2nd layer to be finished.
-        m_second_layer_things_done = true;
+        //m_second_layer_things_done = true;
     }
 
     // Map from extruder ID to <begin, end> index of skirt loops to be extruded with that extruder.
