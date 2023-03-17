9018118d676d909510285bc29bafb44f76824d94
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-03-17 16:53:44 +0000

    errore idiota numero 2

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -2082,9 +2082,12 @@ LayerResult GCode::process_layer(
                 int temp_range = print.config().layer_temperature.get_at(extruder.id());
                 int range_min = print.config().layer_range_min.get_at(extruder.id());
                 int range_max = print.config().layer_range_max.get_at(extruder.id());
+                if (temperature > 0 && (temperature != print.config().first_layer_temperature.get_at(extruder.id())))
+                    range_min < m_layer_index && m_layer_index < range_max ? gcode += m_writer.set_temperature(temp_range, false, extruder.id()) : gcode += m_writer.set_temperature(temperature, false, extruder.id());
+            
             }
             if (temperature > 0 && (temperature != print.config().first_layer_temperature.get_at(extruder.id())))
-                range_min < m_layer_index && m_layer_index < range_max ? gcode += m_writer.set_temperature(temp_range, false, extruder.id()) : gcode += m_writer.set_temperature(temperature, false, extruder.id());
+                 gcode += m_writer.set_temperature(temperature, false, extruder.id());
             
         }
         gcode += m_writer.set_bed_temperature(print.config().bed_temperature.get_at(first_extruder_id));
