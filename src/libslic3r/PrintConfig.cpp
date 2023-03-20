fdc69f28b8ceafdef2b1d53bda84aa32ced5866b
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-03-20 08:25:11 +0000

    correzione dati print config

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -2934,24 +2934,24 @@ void PrintConfigDef::init_fff_params()
     def->set_default_value(new ConfigOptionInts { 200 });
 
     def = this->add("layer_temperature", coInts);
-    def->label = L("Other layers");
-    def->tooltip = L("Nozzle temperature for layers after the first one. Set this to zero to disable "
-                     "temperature control commands in the output G-code.");
+    def->label = L("new temperature");
+    def->tooltip = L("min layer of the range. Set it to zero to disable"
+                       "temperature control commands in the output G-code.");
     def->sidetext = L("°C");
     def->full_label = L("Nozzle layer temperature");
     def->min = 0;
     def->max = max_temp;
-    def->set_default_value(new ConfigOptionInts { 200 });
+    def->set_default_value(new ConfigOptionInts { 0 });
 
     def = this->add("layer_range_min", coInts);
     def->label = L("range min");
-    def->tooltip = L("Nozzle temperature for layers after the first one. Set this to zero to disable "
+    def->tooltip = L("max layer of the range. Set this to zero to disable "
                      "temperature control commands in the output G-code.");
     def->sidetext = L("");
     def->full_label = L("layer range min");
     def->min = 0;
     def->max = max_temp;
-    def->set_default_value(new ConfigOptionInts { 200 });
+    def->set_default_value(new ConfigOptionInts { 0 });
 
     def = this->add("layer_range_max", coInts);
     def->label = L("range max");
@@ -2961,7 +2961,7 @@ void PrintConfigDef::init_fff_params()
     def->full_label = L("layer range max");
     def->min = 0;
     def->max = max_temp;
-    def->set_default_value(new ConfigOptionInts { 200 });
+    def->set_default_value(new ConfigOptionInts { 0 });
 
     def = this->add("thick_bridges", coBool);
     def->label = L("Thick bridges");
